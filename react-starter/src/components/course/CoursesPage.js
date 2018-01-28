8b3e2b35364bf4dcc66d1a86b5b0c78d126b6866
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-28 02:24:00 -0500

    create a simple form to add courses in CoursesPage

--- react-starter/src/components/course/CoursesPage.js
+++ react-starter/src/components/course/CoursesPage.js
@@ -1,12 +1,33 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
-
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: { title: "" }
+    };
+  }
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course})
+  }
+  onClickSave(){
+    alert(`Save ${this.state.course.title}`);
+  }
   render() {
 
     return (
       <div>
         <h1>Courses</h1>
+        <input
+        type="text"
+        onChange={this.onTitleChange}
+        value={this.state.course.title}
+        />
+        <input type="submit"
+               value="Save"
+               onClick={this.onClickSave}/>
     </div>
     );
   }
